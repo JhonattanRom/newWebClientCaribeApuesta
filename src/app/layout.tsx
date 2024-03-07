ebcbb4f0e581737479a9d24a29b2bc9a521225af
Jhonattan <jhonattanromdev@gmail.com>
2024-03-07 08:50:22 -0400

    Fix: positionating slick-carousel styles on principal layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { Login } from "./_ui/Login";
 import { NavigationMenu } from "./_ui/NavigationMenu";
 import 'normalize.css/normalize.css';
 import Link from "next/link";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const metadata: Metadata = {
   title: "CaribeApuesta",
