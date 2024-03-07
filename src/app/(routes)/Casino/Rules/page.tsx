5092c060f9b7bd6e1116d45ac66bf7553f9d9ecb
Jhonattan <jhonattanromdev@gmail.com>
2024-03-07 10:04:29 -0400

    feat: navigation responsive items

--- src/app/(routes)/Casino/Rules/page.tsx
+++ src/app/(routes)/Casino/Rules/page.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function Rules() {
     return (
         <div className={styles.container}>
-            <h2> RULES </h2>
+            <h2> REGLAS </h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta earum repellendus suscipit maiores, reiciendis maxime nulla modi distinctio possimus recusandae voluptatum accusamus quia quae ex aperiam dolores excepturi odio officiis inventore,
                 architecto, numquam vitae voluptas. Hic nostrum natus ullam cupiditate eligendi. Ad perferendis tempora quos? Mollitia facilis voluptas et cum?</p>
         </div>
