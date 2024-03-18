3a6d6fce67d433c3bba4df444d69471369963d46
JhonattanRom <jhonattanromdev@gmail.com>
2024-03-18 09:40:57 -0400

    fix: try #9

--- src/app/(routes)/HorseRacing/Rules/page.tsx
+++ src/app/(routes)/HorseRacing/Rules/page.tsx
@@ -313,7 +313,7 @@ export default function Rules() {
                         <table>
                             <thead>
                                 <tr>
-                                    <td colSpan={2}> REGLAS DE OPERACIÓN PARA LOS HIPÓDROMOS CLASE "A" </td>
+                                    <td colSpan={2}> REGLAS DE OPERACIÓN PARA LOS HIPÓDROMOS CLASE 'A' </td>
                                 </tr>
                             </thead>
                             <tbody>
